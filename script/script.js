4b3e2262f94638da58ddbecfcddd0e2a723dc15b
Nikita Kulichev <longrast.2002@gmail.com>
2023-01-25 22:15:28 +0300

    git init

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const start = document.getElementById("start");
+const end = document.getElementById("end");
+const submitButton = document.getElementById("submit");
+const ratings = document.querySelectorAll("state-start__choose-button");
+const actualRating = document.getElementById("stars");
+
+submitButton.addEventListener("click", () => {
+  start.style.display = "none";
+  end.style.display = "flex";
+});
+
+function myFunction1() {
+  actualRating.innerHTML = "1";
+}
+function myFunction2() {
+  actualRating.innerHTML = "2";
+}
+function myFunction3() {
+  actualRating.innerHTML = "3";
+}
+function myFunction4() {
+  actualRating.innerHTML = "4";
+}
+function myFunction5() {
+  actualRating.innerHTML = "5";
+}
